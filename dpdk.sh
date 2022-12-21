b1e0178c795be9dc4698f773953adea4cb44ee17
Jiawei Xiao <jiawxiao@gmail.com>
2022-12-21 22:31:23 +0800

    build dpdk on linux 5.14 with gcc 9.4

--- dpdk.sh
+++ dpdk.sh
@@ -13,9 +13,11 @@ patch -p 1 -d dpdk/ < ixgbe_19_11.patch
 if lspci | grep -q 'ConnectX-5'; then
    patch -p 1 -d dpdk/ < mlx5_19_11.patch
 elif lspci | grep -q 'ConnectX-3'; then
-    patch -p 1 -d dpdk/ < mlx4_18_11.patch
+    patch -p 1 -d dpdk/ < mlx4_19_11.patch
     sed -i 's/CONFIG_RTE_LIBRTE_MLX4_PMD=n/CONFIG_RTE_LIBRTE_MLX4_PMD=y/g' dpdk/config/common_base
 fi
+
+patch -p1 -N -d dpdk/ < linux_5_14_gcc_9_4.patch
 
 # Configure/compile dpdk
 make -C dpdk/ config T=x86_64-native-linuxapp-gcc
